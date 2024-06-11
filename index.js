e3eeb0ccff641b2d37717b6601bd2c9d89edbed6
elmo <135143680+pipocodis@users.noreply.github.com>
2024-06-11 15:37:29 +0300

    Make button add content of input to list

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const input = document.querySelector("#input");
 const button = document.querySelector("#addButton");
 const list = document.querySelector("#list");
+
+button.addEventListener("click", () => {
+  const inputContent = input.value;
+  input.value = "";
+
+  const item = document.createElement("li");
+  const text = document.createElement("span");
+
+  item.appendChild(text);
+  text.textContent = inputContent;
+  text.style.color = "white";
+  list.appendChild(item);
+
+  input.focus();
+});
